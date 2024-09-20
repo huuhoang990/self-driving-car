d35805beb1fc1f57283ef59b49a78d01d5c23d85
huuhoang990 <huuhoang990@gmail.com>
2024-09-20 19:36:24 +0900

    add sensor

--- car.js
+++ car.js
@@ -10,12 +10,13 @@ class Car {
         this.maxSpeed = 3;
         this.friction = 0.05;
         this.angle = 0;
-
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update() { 
         this.#move();
+        this.sensor.update();
     }
 
     #move() {
@@ -76,5 +77,6 @@ class Car {
         );
         ctx.fill();
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
